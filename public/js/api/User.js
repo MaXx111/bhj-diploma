8c36fb1753e8e11b2cb7c22901da1ba4102b00c1
Sidorov Maksim <maxx11167@gmail.com>
2022-11-15 19:02:14 +0300

    Update API #1

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -39,7 +39,20 @@ class User {
    * Получает информацию о текущем
    * авторизованном пользователе.
    * */
-  static fetch(callback) {
+  static fetch(callback) {  
+    createRequest({
+      url: `${this.URL}/current`,
+      method: "GET",
+      callback: (err, response) => {
+        if (response && response.user) {
+          this.setCurrent(response,user);
+        } else {
+          console.log(err)
+          this.unsetCurrent();
+        }
+        callback(err, response);
+      }
+    })
 
   }
 
@@ -70,15 +83,50 @@ class User {
    * сохранить пользователя через метод
    * User.setCurrent.
    * */
-  static register(data, callback) {
-
+  static register(data, callback) { 
+    createRequest({
+      url: `${this.URL}/register`,
+      method: "POST",
+      data,
+      callback: (err, response) => {
+        if(response && response.user) {
+          this.setCurrent(response.user);
+        } else {
+          console.log(err);
+        }
+        callback(err, callback);
+      }
+    })
   }
+
+  // const data = {
+  //   name: 'Vlad',
+  //   email: 'test@test.ru',
+  //   password: 'abracadabra'
+  // }
+
+  // User.register( data, ( err, response ) => {
+  //   console.log( response.success );
+  //   console.log( err )
+  // });
 
   /**
    * Производит выход из приложения. После успешного
    * выхода необходимо вызвать метод User.unsetCurrent
    * */
   static logout(callback) {
-
+    createRequest({
+      url: `${this.URL}/logout`,
+      method: "POST",
+      callback: (err, response) => {
+        if (response.success === "true") {
+          this.unsetCurrent();
+        }
+      }
+    })
   }
 }
+
+// User.logout((err, response) => {
+//  console.log(response);
+// });
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -8,69 +8,35 @@
 const createRequest = (options = {}) => {
     const xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-    let err;
-    try { 
-        if(options.method !== "GET") {
-            let dataUrl = Object.entries(options.data);
-            let formData = new FormData();
-            for (const [key, value] of dataUrl) {
-                formData.append(key, value);
-            }
-            xhr.open( options.method, options.url);
-            xhr.send(formData);
-        }
+    let formData;
+    let getUrl;
 
-        if (options.method === "GET") {
-            let getData = options.data;
-            let getUrl = `${options.url}?mail=${getData.email}&password=${getData.password}`;
-            xhr.open(options.method, getUrl);
-            xhr.send();
+    if (options.method === "GET") {
+        if (!options.data) {
+            getUrl = options.url
+        } else {
+            getUrl = "?";
+            for (let key in options.data) {
+                getUrl += `${key}=${options.data[key]}&`;
+            };
         }
-    } catch (error){
-        err =  'Ошибка ' + error.name + ":" + error.message + "\n" + error.stack;
+    } else if (options.method !== "GET") {
+        getUrl =options.url;
+        let dataUrl = Object.entries(options.data);
+        formData = new FormData();
+        for (const [key, value] of dataUrl) {
+            formData.append(key, value);
+        }
+    }
+
+    try { 
+        xhr.open(options.method, getUrl);
+        xhr.send(formData);
+    } catch (err){
+        options.callback(err, null);
     }
 
     xhr.onload = () => {
-        if (err) {
-            options.callback(err, null);
-        } else {
             options.callback(null, xhr.response);
-        }
     }
 };
-
-
-
-
-// const createRequest = (options = {}) => {
-//     const xhr = new XMLHttpRequest();
-//     console.log(options);
-//     xhr.responseType = "json"
-//     let url;
-//     let method;
-//     let data;
-//     if (options.method) {
-//         data = Object.entries(options.data);
-//         method = options.method;
-//         url = options.url;
-//         if (method === 'GET') {
-//             for ([key, value] of data) {
-//                 url += key + '=' + value + '&';
-//                 console.log(url += key + '=' + value + '&');
-//             }
-//             xhr.open(method, url);
-//             xhr.send();
-//         }
-//         if (method !== 'GET') {
-//             formData = new FormData();
-//             for ([key, value] of data) {
-//                 formData.append(key, value);
-//             }
-//             xhr.open(method, url);
-//             xhr.send(formData);
-//         }
-//     }   
-// }
-
-
-
